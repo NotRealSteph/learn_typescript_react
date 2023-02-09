486a870610b5a4fa9230274c51a2b6ff401cc2a6
stevetafe <steven.lau2@studytafensw.edu.au>
2023-02-09 15:14:48 +1100

    added component and proptypes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import "./App.css";
 import pokemon from "./pokemon.json"
+import PropTypes from "prop-types";
+
+const PokemonRow = ({pokemon}) => (<tr>
+            <td>{pokemon.name.english}</td>
+            <td>{pokemon.type.join(', ')}</td>
+          </tr>);
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+}
 
 function App() {
   return (
@@ -20,11 +35,8 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {pokemon.map((pokemon) => (
-            <tr>
-            <td>{pokemon.name.english}</td>
-            <td>{pokemon.type.join(', ')}</td>
-          </tr>
+          {pokemon.slice(0,20).map((pokemon) => (
+            <PokemonRow pokemon = {pokemon} key={pokemon.id} />
             ))}
         </tbody>
       </table>
